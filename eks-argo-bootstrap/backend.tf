83a9ba749859a6ceaa06de35500b088c3cd97aa9
mrahmani <mrahmani@salesforce.com>
2025-12-08 11:16:20 -0500

    adding working bootstrap

--- eks-argo-bootstrap/backend.tf
+++ eks-argo-bootstrap/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "mehdi-platform-tf-state"
-    key            = "target-cluster/cluster-infra/terraform.tfstate"
+    key            = "eks-atlantis-boostrap/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock-table"
     encrypt        = true
