028d672e34b4982c4b9dfbbf804585c553b6e565
mrahmani <mrahmani@salesforce.com>
2026-01-14 22:52:07 -0500

    adding fixes

--- svc-cluster1/main.tf
+++ svc-cluster1/main.tf
@@ -35,9 +35,9 @@ resource "aws_eks_node_group" "svc_cluster1_nodes" {
   instance_types = ["t3.medium"] 
 
   scaling_config {
-    desired_size = 1
-    max_size     = 1
-    min_size     = 1
+    desired_size = 2
+    max_size     = 2
+    min_size     = 2
   }
 
   depends_on = [
